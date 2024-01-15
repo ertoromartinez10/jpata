827674e582ff0df717ce93a9b260fbba04eac2a0
ertoromartinez10 <156683516+ertoromartinez10@users.noreply.github.com>
2024-01-15 16:49:47 +0100

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+int a = 87654 / 54637 ; 
+int b = 7+4+4+4+4+4+4+4+4+4+4+44+4+4+4+4+4+4+4+4+4+4+4+4+4+4+44+4+4+4+4+4+4+4+4+4+4+4+4+4+4+4+4+4+4+4+44+44+44+4+4+4+4+4+44+4+4+4+4+4+4+4+4+4+4+4+4+4+4+4; 
+int somma, differenza;
+float divisione;
+somma = a + b;
+differenza = a - b; 
+divisione = a / b;
+printf("%d\n", a + b);
+printf("%d\n", a - b);
+printf("%f\n", divisione);
+return 0;
+}
